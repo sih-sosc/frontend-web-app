95e9bb8407aa6ff28ddf8919ed2858c3bccf70bf
Musthaq Ahamad <musthu.gm@gmail.com>
2019-02-28 12:35:24 +0530

    Change Login UI

--- src/Pages/Loginpage/LoginPage.js
+++ src/Pages/Loginpage/LoginPage.js
@@ -2,50 +2,71 @@ import React, { Component } from 'react';
 
 import auth from '../../auth'
 import history from '../history';
-import './loginPage.css'
+import styles from './loginPage.module.css'
 
 class LoginPage extends Component {
-  state={
-    todash:false,
-  }
-  
 
-async onsubmit(){
-  const email = document.getElementById("username").value;
-  const password = document.getElementById("password").value; 
-  
-  const userdetails={
-    email:email,
-    password:password,
-  };
-  if( auth.login_check(userdetails))
-  {
-    history.push('/dashboard')
-  }
-  
-} 
-register(){
-  history.push('/Register')
-}
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      email: "",
+      password: ""
+    }
+    this.onsubmit = this.onsubmit.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+
+  }
+
+  handleEmail = function(event) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  handlePassword = function(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  onsubmit = async function() {
+    
+    const userdetails={
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    console.log(userdetails);
+
+    if( auth.login_check(userdetails))
+    {
+      history.push('/dashboard')
+    }
+  }
 
 
   render() {
-   
   
     return (
-      <div className="main" >
-      
-      <h1 >login</h1>
-      <text>name</text>
-      <input id="username"></input>
-      <br/>
-      <text>Password</text>
-      <input id="password"></input>
-      <br/>
-      <button onClick={this.onsubmit}>submit</button>
-      <button onClick={this.register}>register</button>
-      
+      <div className={styles.loginPage} >
+        <div className={styles.card}>
+          <h1 >Sign In</h1>
+          <input type="text" 
+            id="username" 
+            value={this.state.email}
+            onChange={this.handleEmail} 
+            placeholder="example@email.com"></input>
+          <br/>
+          <input type="password" 
+            id="password" 
+            value={this.state.password} 
+            onChange={this.handlePassword}
+            placeholder="Password"></input>
+          <br/>
+          <button className={styles.login} onClick={this.onsubmit}>SIGN IN</button>
+        </div>      
       </div>
     );
   }
