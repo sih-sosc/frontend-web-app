2376040eefae6e217100f3c49d22a1d278841a6d
Nathaniel <nathanielmat2@gmail.com>
2019-03-02 08:31:55 +0530

    Created NavBar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
     <div className={Styles.NavBar}>
-      <h2>AICTE Conveyer</h2>
+      <h2>AICTE Conveyor</h2>
       <div className={Styles.MyProfile}>
         <div className={Styles.ProfilePic}></div>
         <a  className={Styles.MyName} href="home.asp">MyName</a>
