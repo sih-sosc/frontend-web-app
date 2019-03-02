2fb6c72e5214ef9cca6cd4115a26f85fb8a79d95
Rumaan <rumaankalander@gmail.com>
2019-03-03 03:39:04 +0530

    Merge changes

--- src/components/LeftNav/index.js
+++ src/components/LeftNav/index.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import Styles from './styles.module.css';
+import React, { Component } from "react";
+import Styles from "./styles.module.css";
 import {
   BrowserRouter as Router,
   Link,
   Route // for later
-} from 'react-router-dom'
+} from "react-router-dom";
 
 class LeftNav extends Component {
-
   render() {
     return (
-    <div className={Styles.leftNav}>
-      <ul>
-        <li>Dashboard</li>
-        <li>E-mail</li>
-        <li>SMS</li>
-        <li>Contact List</li>
-        <li>Services</li>
-        <li>Users</li>
-        <li>Settings</li>
-      </ul> 
-    </div>
+      <div className={Styles.leftNav}>
+        <ul>
+          <li>Dashboard</li>
+          <li>E-mail</li>
+          <li>SMS</li>
+          <li>Contact List</li>
+          <li>Services</li>
+          <li>Users</li>
+          <li>Settings</li>
+        </ul>
+      </div>
     );
   }
 }
