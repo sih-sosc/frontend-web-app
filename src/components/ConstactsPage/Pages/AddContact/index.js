8f579eb07a4eb6d26ff68494aca979d8ebecc0a0
Musthaq Ahamad <musthu.gm@gmail.com>
2019-03-03 08:31:13 +0530

    update

--- src/components/ConstactsPage/Pages/AddContact/index.js
+++ src/components/ConstactsPage/Pages/AddContact/index.js
@@ -1,45 +1,88 @@
 import React, { Component } from 'react';
 import Styles from './styles.module.css';
-import {OutTable, ExcelRenderer} from 'react-excel-renderer';
+import { OutTable, ExcelRenderer } from 'react-excel-renderer';
+import axios from 'axios'
 
 class ContactList extends Component {
-      
-fileHandler(event) {
-    let fileObj = event.target.files[0];
-  
-    //just pass the fileObj as parameter
-    ExcelRenderer(fileObj, (err, resp) => {
-      if(err){
-        console.log(err);            
-      }
-      else{
-        
-        console.log(resp);
-    //     const  merged = [].concat.apply([], data);
-    //     console.log(merged);
-    //    document.getElementById("mails").value= data;
-        
-      }
-    });
-       
+  constructor(props) {
+    super(props)
+
+
+
+
+    this.state = {
+      getinfo: false,
+      name: "",
+      ContactList: null,
+    }
   }
 
-    constructor(props) {
-        super(props)
+  handlename = (event) => {
+    this.setState({
+      name: event.target.value,
+    })
+  }
+
+  sendContact() {
+    var contactdetails = {
+      name: this.state.name,
+      contacts: this.state.ContactList,
     }
+    console.log(contactdetails);
+    var token = localStorage.getItem('token');
+    axios.request({
+      method: "POST",
+      url: "http://172.17.104.204:3000/contact_list/new",
 
-    render() {
-        return(
-            <div>
-            <h1>Hello</h1>
-            <input id="mails" value="hey" ></input>
-        
-         <input type="file" onChange={this.fileHandler} style={{"padding":"10px"}} />
-         </div>
-        )
+      data: { 'token':token,
+              'listName':contactdetails.name,
+              'data':contactdetails.contacts,
+               },
 
 
-    }
+    });
+  }
+
+  setContactList = (resp) => { this.setState({ ContactList: resp }); }
+
+  fileHandler = (event, setContactList ) => {
+    let fileObj = event.target.files[0];
+
+    //just pass the fileObj as parameter
+    ExcelRenderer(fileObj, (err, resp) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+
+       setContactList(resp.rows);
+
+      }
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+
+        <div>
+          <div>Add Contact List</div>
+          <div>
+            Name
+            <input id="name" onChange={this.handlename}></input>
+          </div>
+          <input id="mails" ></input>
+          <input type="file" onChange={ (e) => {this.fileHandler(e, this.setContactList)}} style={{ "padding": "10px" }} />
+          {this.state.ContactList !== null ? this.sendContact() : <div />}
+        </div>
+        <button>Add ContactList</button>
+
+      </div>
+    )
+
+
+  }
 }
 
 export default ContactList;
